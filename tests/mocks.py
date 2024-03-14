3fb63cf83d35ba264df753e5b812ccc0edd3a49f
cortadocodes <cortado.codes@protonmail.com>
2024-03-14 15:26:50 +0000

    TST: Test cloud function

---
+++ tests/mocks.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MockCloudEvent:
+    data: dict
+
+
+# This class is duplicated from `data-gateway` (see ATTRIBUTIONS.md).
+class MockBigQueryClient:
+    def __init__(self, expected_query_results=None):
+        self.expected_query_results = expected_query_results or [[]]
+        self.inserted_rows = []
+        self.queries = []
+        self._next_query_result_index = 0
+
+    def get_table(self, name):
+        """Do nothing.
+
+        :param str name:
+        :return None:
+        """
+        pass
+
+    def insert_rows(self, table, rows):
+        """Append the given rows to the `inserted_rows` attribute.
+
+        :param str table:
+        :param list(dict) rows:
+        :return None:
+        """
+        self.inserted_rows.append(rows)
+
+    def query(self, query, *args, **kwargs):
+        """Return the next value in the `expected_query_results` attribute as a `MockQueryResult` instance.
+
+        :param str query:
+        :return MockQueryResult:
+        """
+        self.queries.append(query)
+
+        try:
+            result = MockQueryResult(result=self.expected_query_results[self._next_query_result_index])
+        except IndexError:
+            raise ValueError(
+                "More mock queries have been run than mock query results given - make sure you've given enough in the "
+                f"{type(self).__name__} constructor."
+            )
+
+        self._next_query_result_index += 1
+        return result
+
+
+# This class is duplicated from `data-gateway` (see ATTRIBUTIONS.md).
+class MockQueryResult:
+    def __init__(self, result):
+        self._result = result
+
+    def result(self):
+        """Return the `self._result` attribute.
+
+        :return any:
+        """
+        return self._result
---
+++ tests/test_event_handler.py
@@ -0,0 +1,62 @@
+import base64
+import os
+import unittest
+from unittest.mock import patch
+
+from functions.event_handler.main import store_pub_sub_event_in_bigquery
+from tests.mocks import MockBigQueryClient, MockCloudEvent
+
+
+REPOSITORY_ROOT = os.path.dirname(os.path.dirname(__file__))
+
+
+# # Manually add the cloud_functions package to the path (its imports have to be done in a certain way for Google Cloud
+# # Functions to accept them that doesn't work when running/testing the package locally).
+# sys.path.insert(0, os.path.abspath(os.path.join(REPOSITORY_ROOT, "functions", "event_handler")))
+# from functions.event_handler.main import store_pub_sub_event_in_bigquery
+
+
+class TestEventHandler(unittest.TestCase):
+    def test_store_pub_sub_event_in_bigquery(self):
+        cloud_event = MockCloudEvent(
+            data={
+                "message": {
+                    "data": base64.b64encode(b"some-data"),
+                    "attributes": {
+                        "sender": "octue/test-service:5.6.3",
+                        "sender_type": "PARENT",
+                        "question_uuid": "ca534cdd-24cb-4ed2-af57-e36757192acb",
+                        "version": "1.0.3",
+                        "message_number": "0",
+                    },
+                    "messageId": "1234",
+                    "publishTime": "some-datetime",
+                }
+            }
+        )
+
+        mock_big_query_client = MockBigQueryClient()
+
+        with patch("functions.event_handler.main.Client", return_value=mock_big_query_client):
+            store_pub_sub_event_in_bigquery(cloud_event)
+
+        self.assertEqual(
+            mock_big_query_client.inserted_rows[0][0],
+            {
+                "event": "some-data",
+                "attributes": {
+                    "sender": "octue/test-service:5.6.3",
+                    "sender_type": "PARENT",
+                    "question_uuid": "ca534cdd-24cb-4ed2-af57-e36757192acb",
+                    "version": "1.0.3",
+                    "message_number": "0",
+                },
+                "sender": "octue/test-service:5.6.3",
+                "sender_type": "PARENT",
+                "question_uuid": "ca534cdd-24cb-4ed2-af57-e36757192acb",
+                "version": "1.0.3",
+                "ordering_key": "0",
+                "backend": "GoogleCloudPubSub",
+                "backend_metadata": {"message_id": "1234", "publish_time": "some-datetime", "ordering_key": None},
+            },
+        )
