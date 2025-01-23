f28b15f01900558ac2d24040a0279f7267858261
cortadocodes <cortado.codes@protonmail.com>
2025-01-23 17:35:26 +0000

    FEA: Dispatch questions as Kueue jobs

--- functions/event_handler/main.py
+++ functions/event_handler/main.py
@@ -1,19 +1,22 @@
 import base64
+import copy
 import json
 import logging
 import os
 import sys
 
 import functions_framework
-from google.cloud.bigquery import Client
-
+import kubernetes
+from google.cloud.bigquery import Client as BigQueryClient
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-BIGQUERY_EVENTS_TABLE = os.environ["BIGQUERY_EVENTS_TABLE"]
 BACKEND = "GoogleCloudPubSub"
+BIGQUERY_EVENTS_TABLE = os.environ["BIGQUERY_EVENTS_TABLE"]
+KUEUE_LOCAL_QUEUE_NAME = os.environ["KUEUE_LOCAL_QUEUE_NAME"]
+ARTIFACT_REPOSITORY = os.environ["ARTIFACT_REPOSITORY"]
 
 
 @functions_framework.cloud_event
@@ -33,12 +36,15 @@ def store_pub_sub_event_in_bigquery(cloud_event):
         "ordering_key": cloud_event.data["message"].get("orderingKey"),
     }
 
+    original_event = copy.deepcopy(event)
+    original_attributes = copy.deepcopy(attributes)
+
     row = {
         "datetime": attributes.pop("datetime"),
         "uuid": attributes.pop("uuid"),
         "kind": event.pop("kind"),
         "event": event,
-        # Any attributes not popped below will end up in the `other_attributes` column.
+        # Any attributes not popped will end up in the `other_attributes` column.
         "other_attributes": attributes,
         # Pull out some attributes into columns for querying.
         "parent": attributes.pop("parent"),
@@ -57,10 +63,68 @@ def store_pub_sub_event_in_bigquery(cloud_event):
 
     logger.info("Attempting to store row: %r.", row)
 
-    client = Client()
-    errors = client.insert_rows(table=client.get_table(BIGQUERY_EVENTS_TABLE), rows=[row])
+    bigquery_client = BigQueryClient()
+    errors = bigquery_client.insert_rows(
+        table=bigquery_client.get_table(BIGQUERY_EVENTS_TABLE), rows=[row]
+    )
 
     if errors:
         raise ValueError(errors)
 
     logger.info("Successfully stored row in %r.", BIGQUERY_EVENTS_TABLE)
+
+    if original_event["kind"] == "question":
+        _dispatch_kueue_job(original_event, original_attributes)
+
+
+def _dispatch_kueue_job(event, attributes):
+    kubernetes.config.load_kube_config()
+
+    service_namespace, service_name = attributes["recipient"].split("/")
+    service_name, service_revision_tag = service_name.split(":")
+    job_name = f"{service_namespace}-{service_name}-{service_revision_tag}-question-{attributes['question_uuid']}"
+
+    job_metadata = kubernetes.client.V1ObjectMeta(
+        name=job_name,
+        labels={"kueue.x-k8s.io/queue-name": KUEUE_LOCAL_QUEUE_NAME},
+    )
+
+    args = []
+
+    if event.get("input_values"):
+        args.extend(["--input-values", json.dumps(event["input_values"])])
+
+    if event.get("input_manifest"):
+        args.extend(["--input-manifest", json.dumps(event["input_manifest"])])
+
+    job_template = {
+        "spec": {
+            "containers": [
+                kubernetes.client.V1Container(
+                    image=ARTIFACT_REPOSITORY + "/" + attributes["recipient"],
+                    name=job_name,
+                    command=["octue", "question", "ask", "local"],
+                    args=args,
+                    resources={"requests": {"cpu": 2, "memory": "2Gi"}},
+                )
+            ],
+            "restartPolicy": "Never",
+        }
+    }
+
+    job = kubernetes.client.V1Job(
+        api_version="batch/v1",
+        kind="Job",
+        metadata=job_metadata,
+        spec=kubernetes.client.V1JobSpec(
+            parallelism=1, completions=1, suspend=True, template=job_template
+        ),
+    )
+
+    batch_api = kubernetes.client.BatchV1Api()
+    batch_api.create_namespaced_job("default", job)
+    logger.info(
+        "Dispatched to Kueue: question %r for %r.",
+        attributes["question_uuid"],
+        attributes["recipient"],
+    )
