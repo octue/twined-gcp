933858fdf16d672d84b38ec1079811603379ea7a
cortadocodes <cortado.codes@protonmail.com>
2025-02-24 17:16:58 +0000

    REF: Refactor checking for service revision existence

--- functions/service_registry/main.py
+++ functions/service_registry/main.py
@@ -27,29 +27,25 @@ def handle_request(request):
 
     sruid = f"{suid}:{revision_tag}"
 
-    service_revision_exists = _check_service_revision_existence(
-        sruid=sruid,
-        repository_id=os.environ["ARTIFACT_REGISTRY_REPOSITORY_ID"],
-    )
+    tagged_images = _get_tagged_images(repository_id=os.environ["ARTIFACT_REGISTRY_REPOSITORY_ID"])
 
-    if service_revision_exists:
+    if sruid in tagged_images:
         return ("", 200)
 
     return ("Service revision does not exist", 404)
 
 
-def _check_service_revision_existence(sruid, repository_id):
-    """Check if an image corresponding to the given service revision exists in the artifact registry repository.
+def _get_tagged_images(repository_id):
+    """Get the names of the tagged images from the artifact registry repository.
 
-    :param str sruid: the service revision unique identifier (SRUID) for the service revision
     :param str repository_id: the artifact registry repository ID in "projects/<project-id>/locations/<region>/repositories/<repository-name>" form
-    :return bool: `True` if the service revision exists
+    :return set(str): the names of the tagged images e.g. "octue/my-image:0.1.0"
     """
     repository_id = repository_id.strip("/")
 
     client = artifactregistry_v1.ArtifactRegistryClient()
     request = artifactregistry_v1.ListDockerImagesRequest(parent=repository_id)
-    image_names = set()
+    tagged_images = set()
 
     for image in client.list_docker_images(request=request):
         # Untagged images aren't full service revision images.
@@ -58,6 +54,6 @@ def _check_service_revision_existence(sruid, repository_id):
 
         image_name = urllib.parse.unquote(image.name).split(repository_id + "/dockerImages/")[-1].split("@")[0]
         image_tag = image.tags[0]
-        image_names.add(f"{image_name}:{image_tag}")
+        tagged_images.add(f"{image_name}:{image_tag}")
 
-    return sruid in image_names
+    return tagged_images
