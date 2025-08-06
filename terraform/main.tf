96dc4a76ad12b5063d440eab60b4a186f3ca1e46
cortadocodes <cortado.codes@protonmail.com>
2025-08-06 12:29:43 -0400

    FIX: Add provider configuration, making use of region

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,12 @@ terraform {
 }
 
 
+provider "google" {
+  project     = var.project
+  region      = var.region
+}
+
+
 resource "google_project_service" "cloud_resource_manager" {
   project = var.project
   service = "cloudresourcemanager.googleapis.com"
